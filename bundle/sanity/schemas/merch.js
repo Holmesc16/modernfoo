fc2ebe420cb083a064e8fb5543ef39455d3c6d6b
Holmesc16 <holmesc16@gmail.com>
2020-09-28 17:28:06 -0500

    update schema

--- bundle/sanity/schemas/merch.js
+++ bundle/sanity/schemas/merch.js
@@ -1,4 +1,5 @@
 import { RiShirtLine as shirtIcon } from 'react-icons/ri'
+import { number } from 'prop-types';
 
 export default {
     name: 'merch',
@@ -11,6 +12,12 @@ export default {
             title: 'Merch Name',
             type: 'string',
             description: 'Name of merch item'
+        },
+        {
+            name: 'type',
+            title: 'Merch Type',
+            type: 'string',
+            description: 'Type of merch (shirt, sticker, bandana, etc.)'
         },
         {
             name: 'slug',
@@ -30,6 +37,12 @@ export default {
             }
         },
         {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+            description: 'Brief description of merch item'
+        },
+        {
             name: 'price',
             title: 'Price',
             type: 'number',
@@ -37,5 +50,12 @@ export default {
             validation: Rule => Rule.min(10)
             //@TODO : Add custom input component
         },
+        {
+            name: 'stock',
+            title: 'Stock',
+            type: 1,
+            description: 'Available stock for this merch item',
+            validation: Rule => Rule.max(99)
+        }
     ]
 };
