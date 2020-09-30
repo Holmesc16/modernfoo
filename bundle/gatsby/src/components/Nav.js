627389770a1d568465534c0eb6e76c71b0e85b18
Holmesc16 <holmesc16@gmail.com>
2020-09-30 09:30:42 -0500

    nav border

--- bundle/gatsby/src/components/Nav.js
+++ bundle/gatsby/src/components/Nav.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components'
-import Logo from './Logo'
+import styled from 'styled-components';
+import Logo from './Logo';
+import border from '../assets/images/borders/border_transparent_plum.svg';
 
 const StyledNav = styled.nav`
-    background:white;
-    box-shadow: 0 0 3px 5px rgba(0, 0, 0, 0.044);
+
+    /* background: white; */
+    /* box-shadow: 0 0 3px 5px rgba(0,0,0,0.044); */
     margin-bottom: 3rem;
+    border: 50px solid transparent;
+    border-image: url(${border}) 25% 30% 10% 20% repeat;
+    border-image-slice: 65;
+
     ul {
         margin: 0;
         padding: 0;
@@ -37,7 +43,7 @@ const StyledNav = styled.nav`
 
 const Nav = () => {
     return (
-    <StyledNav>
+    <StyledNav className="border">
         <ul>
             <li>
                 <Link to="/">Home</Link>
