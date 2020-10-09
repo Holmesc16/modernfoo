3724ddb21a43d0eafd735741e0432ece8294c928
Holmesc16 <holmesc16@gmail.com>
2020-10-09 11:14:07 -0500

    allMerch query export

--- bundle/gatsby/src/pages/Merch.js
+++ bundle/gatsby/src/pages/Merch.js
@@ -1,12 +1,38 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 import Layout from '../components/Layout';
 
-const MerchPage = () => {
-    return (
-        <>
-            <p>Merch goes here for sale fuhl</p>
-        </>
-    )
-}
+const MerchPage = (props) => {
+  console.log(props);
+  return (
+    <>
+      <p>Merch goes here for sale fuhl</p>
+    </>
+  );
+};
 
-export default MerchPage
+export const query = graphql`
+  query merchQuery {
+    merch: allSanityMerch {
+      nodes {
+        id
+        slug {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+        name
+        price
+        stock
+        type
+      }
+    }
+  }
+`;
+
+export default MerchPage;
