927bf9d249c76eb6c54e3ba0279b05b359dc5e22
Cade Holmes <holmesc16@gmail.com>
2020-10-10 21:24:28 -0500

    fix merge conflict

---
+++ bundle/sanity/schemas/persons.js
@@ -0,0 +1,39 @@
+import { IoIosPersonAdd as personIcon } from 'react-icons/io'
+import { number } from 'prop-types';
+
+export default {
+    name: 'person',
+    title: 'person',
+    type: 'document',
+    icon: personIcon,
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'slug',
+            type: 'slug',
+            options: {
+                source: 'name',
+                maxLength: 100
+            }
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: 'desription',
+            title: 'Description',
+            type: 'text',
+            description: 'Describe the person'
+        }
+    ]
+};
